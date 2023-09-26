1b4cfb36d9763c1e5848f150d180d0445f482471
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-27 01:44:14 +0300

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_26_223815) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_224208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_223815) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_counter"
+    t.integer "likes_counter"
   end
 
   create_table "users", force: :cascade do |t|
