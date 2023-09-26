34cfaf077db93fb87dd1d0947f7fff063ce2ce96
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-27 01:46:42 +0300

    removed uncessary time stamps

--- db/migrate/20230926224154_add_timestamps_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampsToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :updated_at, :datetime
-    add_column :comments, :created_at, :datetime
-  end
-end
--- db/migrate/20230926224201_add_timestamps_to_likes.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampsToLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_column :likes, :updated_at, :datetime
-    add_column :likes, :created_at, :datetime
-  end
-end
