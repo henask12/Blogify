2c4adadf4a70712418d7b74c616b58ed5110d99c
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-29 00:47:24 +0300

    user model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,13 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
+  after_create :update_user_posts_counter
+  after_destroy :update_user_posts_counter
+
+  def update_user_posts_counter
+    update(posts_counter: posts.count)
+  end
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
