8d1cd2c493cbf8a930ab220e3951ff876ee8ea39
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-28 15:13:02 +0300

    schema update in db

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_224208) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_counter"
   end
 
 end
