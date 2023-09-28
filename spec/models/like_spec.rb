c8fa8ae14e4bb1f23fcf3b5a14761e902a263bde
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-29 00:47:56 +0300

    test for like created

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Henok Mekonnen', photo: 'https://shorturl.at/nrsOQ', bio: 'Developer from Ethiopia.')
+    @post = Post.create(author: @user, title: 'My first post', text: 'This is my first post')
+    @like = Like.create(user: @user, post: @post)
+  end
+
+  describe 'Instance created with valid attributes' do
+    it 'should be valid' do
+      expect(@like).to be_valid
+    end
+  end
+
+  describe 'Instance created with valid attributes should update post likes_counter' do
+
+    it 'should update post likes_counter after destroy' do
+      @like.destroy
+      expect(@post.likes_counter).to eq(0)
+    end
+  end
 end
