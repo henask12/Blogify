c922f1d545bc8918c5232db3b869467e9dcfd5e2
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-10-06 01:50:08 +0300

    linters error fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments, foreign_key: :post_id, dependent: :destroy
   has_many :likes, foreign_key: :post_id, dependent: :destroy
-  
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
   validates :title, length: { maximum: 250 }
- validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   after_create :update_user_posts_counter
   after_destroy :update_user_posts_counter
