431d521372b4c51b6000b26a977d0dc405ca971f
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-10-06 01:50:08 +0300

    linters error fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, foreign_key: :user_id, dependent: :destroy
   has_many :likes, foreign_key: :user_id, dependent: :destroy
-  
+
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
