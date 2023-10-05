273d23aa883d4b1605d2559d8e623f5bf2def0a5
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-10-06 01:24:39 +0300

    update model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,13 +1,13 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, foreign_key: :user_id
+  belongs_to :post, foreign_key: :post_id
 
-  def self.create_like(user, post)
-    Like.create(user: user, post: post)
-  end
-  after_create :update_likes_counter
-  after_destroy :update_likes_counter
-  def update_likes_counter
+  after_create :update_post_likes_counter
+  after_destroy :update_post_likes_counter
+
+  private
+
+  def update_post_likes_counter
     post.update(likes_counter: post.likes.count)
   end
 end
