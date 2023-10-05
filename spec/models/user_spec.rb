984fb193bcd29af1535aac6ec68bf886e1eb6ee2
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-10-06 01:45:47 +0300

    updated user spec file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,8 +46,9 @@ RSpec.describe User, type: :model do
 
   describe 'Methods' do
     it 'should return recent posts' do
-      recent_posts = @user.recent_posts(3)
+      recent_posts = @user.post_recent
       expect(recent_posts).to be_an(ActiveRecord::Relation)
     end
   end
+  
 end
