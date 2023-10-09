185080f08428843f0e386761fcd6a866e6658172
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-10-10 02:13:07 +0300

    feat: Implement user post index page features and tests

--- spec/features/user_index_page_spec.rb
+++ spec/features/user_index_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'User Index Page', type: :feature do
   before do
-    @user = User.create(name: 'César Herrera', photo: 'https://i.pravatar.cc/150?img=3', bio: 'I am a developer')
+    @user = User.create(name: 'Henok Mekonnen', photo: 'https://i.pravatar.cc/150?img=3', bio: 'I am a developer')
     @post1 = Post.create(author_id: @user.id, title: 'My first post', text: 'This is my first post')
     @post2 = Post.create(author_id: @user.id, title: 'My second post', text: 'This is my second post')
   end
@@ -10,7 +10,7 @@ RSpec.feature 'User Index Page', type: :feature do
   # I can see the username of all other users.
   scenario 'shows the username of all other users' do
     visit users_path
-    expect(page).to have_content('César Herrera')
+    expect(page).to have_content('Henok Mekonnen')
     expect(page).to have_content('Number of post: 2')
     expect(page).to have_content('Add a new post')
   end
@@ -30,9 +30,9 @@ RSpec.feature 'User Index Page', type: :feature do
   # When I click on a user, I am redirected to that user's show page.
   scenario 'redirects to the user show page when clicking on a user and see it content' do
     visit users_path
-    first(:link, 'César Herrera').click
+    first(:link, 'Henok Mekonnen').click
     expect(current_path).to eq(user_path(@user.id))
-    expect(page).to have_content('César Herrera')
+    expect(page).to have_content('Henok Mekonnen')
     expect(page).to have_content('I am a developer')
     expect(page).to have_content('My first post')
     expect(page).to have_content('This is my first post')
