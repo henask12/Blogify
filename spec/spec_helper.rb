0a334c51492dd390d232981beff4bc2780f5da38
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-10-10 02:24:02 +0300

    fix: updated post controller

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,12 +13,13 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+  require 'capybara/rspec'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
 
-  require 'capybara/rspec'
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
