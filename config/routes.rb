501de45d931a0006ec0ecc0597b659a69af02792
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-10-13 01:27:16 +0300

    updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:show, :index] do
-    resources :posts, only: [:show, :index, :new, :create] do
-      resources :comments, only: [:new, :create]
+    resources :posts, only: [:show, :index, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
       # Paginate post with kaminari
       get '/page/:page', action: :index, on: :collection
