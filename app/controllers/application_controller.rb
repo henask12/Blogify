03d2ae1c67ec3b9ca986df0e65f30bbb1e7d8d11
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-10-12 23:05:42 +0300

    fix: linters error fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+
   def current_user
     @user = User.first
   end
