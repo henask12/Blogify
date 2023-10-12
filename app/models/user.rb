e7cfecf6c26cf418d53c1876fa357b863121cdcd
Henok Mekonnen <Henokmekonnen@gmail.com>
2023-10-12 21:03:21 +0300

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :confirmable,
+  :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
 
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  # New fields
+  attr_accessor :email, :encrypted_password, :reset_password_token,
+                :reset_password_sent_at, :remember_created_at,
+                :confirmation_token, :confirmed_at, :confirmation_sent_at,
+                :unconfirmed_email
 
   # Returns the 3 most recent posts
   def post_recent
