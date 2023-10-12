dc4ecba988bbfb73bb1419ca233220614eb5c599
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-10-12 22:20:48 +0300

    fix: linters error fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
-  :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
